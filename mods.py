08dd9156132336ab785173324866c5d48b4130d1
702361946 <702361946@qq.com>
2024-08-12 13:09:55 +0800

    #mods
    mods模板

---
+++ mods.py
@@ -0,0 +1,125 @@
+#  Copyright (c) 2024.
+
+# 在此留下作者信息
+
+# import丢这
+def mods_import():
+    pass
+
+
+# 补充定义(变量丢这)
+def mods_supplement():
+    pass
+
+
+# 执行补充(勿改)
+mods_import()
+mods_supplement()
+
+
+# 开始页-开始-前 进游戏->1部分
+def mods_open_game_up():
+    pass
+
+
+# 开始页-开始-后 进游戏->1部分
+def mods_open_game_down():
+    pass
+
+
+# 开始页-记录-前 进游戏->0部分
+def mods_open_game_result_up():
+    pass
+
+
+# 开始页-记录-后 进游戏->0部分
+def mods_open_game_result_down():
+    pass
+
+
+# game页开始前
+def mods_game_open_up():
+    pass
+
+
+# 回合开始前
+def mods_game_time_up():
+    pass
+
+
+# 回合用户输入前
+def mods_game_time_user_up():
+    pass
+
+
+# 骰子页(可以行注释掉循环(game里面)) 前
+def mods_game_dice_up():
+    pass
+
+
+# 骰子页 后
+def mods_game_dice_down():
+    pass
+
+
+# 查看板块前
+def mods_game_plate_reminder_up():
+    pass
+
+
+# 查看板块后
+def mods_game_plate_reminder_down():
+    pass
+
+
+# 查看板块经验 前
+def mods_game_plate_xpg_up():
+    pass
+
+
+# 查看板块经验 后
+def mods_game_plate_xpg_down():
+    pass
+
+
+# 药水 前
+def mods_game_potion_up():
+    pass
+
+
+# 药水 后
+def mods_game_potion_down():
+    pass
+
+
+# 查看怪物意图 前
+def mods_game_monster_adt_up():
+    pass
+
+
+# 查看怪物意图 后
+def mods_game_monster_adt_down():
+    pass
+
+
+# 下一回合
+def mods_game_time_down():
+    pass
+
+
+# 回合结束总结 前
+def mods_game_time_if_up():
+    pass
+
+
+# 回合结束总结 后
+def mods_game_time_if_down():
+    pass
+
+
+# 结束页
+def mods_end():
+    pass
+
+
+# 其他的自己定义自己插吧(qwq
